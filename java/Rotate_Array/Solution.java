98e9ba0e2d4369ce82c2596d1b662d5dd8a71c43
Soujanya_S <52788144+soujanya-bhatt@users.noreply.github.com>
2023-08-28 22:37:04 +0530

    Create Solution.java

---
+++ java/Rotate_Array/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public void reverse(int[] nums, int start, int end){
+        while(start<end){
+            int t = nums[start];
+            nums[start] = nums[end];
+            nums[end] = t;
+            start++;
+            end--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+}
